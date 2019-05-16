5b68ab258365067ff1d251a417be3d929d242f51
nnalge123 <nathannagle1@gmail.com>
2019-05-16 17:03:20 -0400

    playing around

--- themes/inhabitent/404.php
+++ themes/inhabitent/404.php
@@ -12,7 +12,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<section class="error-505 not-found">
+			<section class="error-404 not-found">
 				<header class="page-header">
 					<h1 class="page-title"><?php echo esc_html( 'Oops! That page can&rsquo;t be found.' ); ?></h1>
 				</header><!-- .page-header -->
